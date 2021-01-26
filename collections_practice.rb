261ba0018dd7f01ba1704b063e2ce2a092d2fc4d
Kiana Long <kianajade42.kl@gmail.com>
2021-01-26 15:50:07 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -3,7 +3,9 @@ def sort_array_asc(integers)
   integers.sort
 end
  def sort_array_desc(integers)
-
+   integers.sort do | left, right|
+      right <=> left
+    end
 end
 
 def sort_array_char_count(integers)
