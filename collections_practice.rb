b5effa38f0449f24ebba21d078d0638aec939786
Kiana Long <kianajade42.kl@gmail.com>
2021-01-26 15:47:43 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,8 +1,35 @@
-integers=[7,9,4,3,8,6,5]
+integers=[7,9,4,3,"one","two","six"]
 def sort_array_asc(integers)
   integers.sort
 end
  def sort_array_desc(integers)
-   integers.sort.reverse_array
+    
+end
 
- end
+def sort_array_char_count(integers)
+
+end
+
+def swap_elements(integers)
+
+end
+
+def reverse_array(integers)
+
+end 
+
+def kesha_maker
+
+end 
+
+def find_a(integers)
+
+end
+
+def sum_array(integers)
+
+end
+
+def add_s(integers)
+
+end 
